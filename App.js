3b18b6ca1f0f4a2ee77fe66015a7f7b2f9be8721
Julia Lim <juliamlim@hotmail.com>
2017-12-19 10:23:54 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,6 @@
+let LimboUI;
+
+LimboUI.Button = require('./app/el/Button');
+LimboUI.Input = require('./app/el/Input');
+
+module.exports = LimboUI;
---
+++ index.js
@@ -0,0 +1,6 @@
+import LimboUI from './App';
+import Button from './app/el/Button';
+import Input from './app/el/Input';
+
+export default LimboUI;
+export { Button, Input };
